9e1d11cfcc4ad0617a74725da769ae2341f267e8
arafat2014 <arafatsharif7890@gmail.com>
2025-05-25 14:54:30 +0600

    Load pdf

--- vector_database.py
+++ vector_database.py
@@ -3,3 +3,20 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 
+# Define directory for PDF storage
+PDF_DIRECTORY = 'pdfs/'
+
+def upload_pdf(file):
+    with open(pdfs_directory + file.name, "wb") as f:
+        f.write(file.getbuffer())
+
+
+def load_pdf(file_path):
+    loader = PDFPlumberLoader(file_path)
+    documents = loader.load()
+    return documents
+
+# Load a sample file
+file_path ='udhr_booklet_en_web.pdf'
+documents = documents = load_pdf(file_path)
+print(len(documents))
