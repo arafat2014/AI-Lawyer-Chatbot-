56f2f774b166ef491dae1705396d700b48c53ad5
arafat2014 <arafatsharif7890@gmail.com>
2025-05-25 14:29:23 +0600

    importing modules

---
+++ vector_database.py
@@ -0,0 +1,5 @@
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+
