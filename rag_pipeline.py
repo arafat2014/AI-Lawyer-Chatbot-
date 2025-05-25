165105daea57440e35e8c831037a2b88c380f777
arafat2014 <arafatsharif7890@gmail.com>
2025-05-25 19:55:33 +0600

    Retrieve Documents

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -14,3 +14,9 @@ def retrieve_docs(query: str):
     Perform similarity search on the vector database using the user query.
     """
     return faiss_db.similarity_search(query)
+
+def get_context(documents) -> str:
+    """
+    Combine content from relevant documents into a single context string.
+    """
+    return "\n\n".join(doc.page_content for doc in documents)
