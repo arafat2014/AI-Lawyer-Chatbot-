35327126d4bcfb9eee5963ae328b20519cf12eca
arafat2014 <arafatsharif7890@gmail.com>
2025-05-25 20:48:31 +0600

    Answer Question with Custom Prompt

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -20,3 +20,24 @@ def get_context(documents) -> str:
     Combine content from relevant documents into a single context string.
     """
     return "\n\n".join(doc.page_content for doc in documents)
+
+# Step 3: Answer Question with Custom Prompt
+PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you don’t know the answer, just say that you don’t know — don’t try to make up an answer.
+Do not provide anything outside the given context.
+
+Question: {question}
+Context: {context}
+Answer:
+"""
+def answer_query(documents, model, query: str) -> str:
+    """
+    Generate an answer using the language model and retrieved documents.
+    """
+    context = get_context(documents)
+    prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    chain = prompt | model
+    return chain.invoke({"question": query, "context": context})
+
+
